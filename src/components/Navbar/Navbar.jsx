79ba08088b473199e822cb19054c421d95854191
Peedee01 <piethein@schouten.nl>
2024-12-03 16:57:22 +0100

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,22 @@
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
-// import Logo from "../../assets/IronHub.svg";
+import Logo from "../../assets/Ironhub-logo.png";
 import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>
+    <div className="navbar-container">
       <nav className="navbar">
         <div className="logo">
-          <NavLink>
-            {/* <img src={Logo} alt="ironhub-logo" /> */}
+          <NavLink to={"/"}>
+            <img width={120} src={Logo} alt="ironhub-logo" />
           </NavLink>
         </div>
         <ul className="links">
-          <NavLink to="/">
-            <li>About</li>
-          </NavLink>
-          <NavLink>
+          <NavLink to={"/auth"}>
             <li>Signup</li>
           </NavLink>
-          <NavLink>
+          <NavLink to={"/auth"}>
             <li>Login</li>
           </NavLink>
         </ul>
