cd9277b4b062e2a4bc2cbf8f40251967cd88580c
Nigel Ferreres <nferrefe@gmail.com>
2024-12-03 16:54:01 +0100

    homepage html

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,29 @@
-import "./HomePage.css"
+import "./HomePage.css";
+import heroGif from "../../assets/HeroGif.gif";
+import { useNavigate } from "react-router-dom";
 
-function HomePage(){
-    return(
-        <h1>Home Page</h1>
-    )
+function HomePage() {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => navigate("/about");
+
+  return (
+    <div className="homepage-container">
+      <img id="gif" src={heroGif} alt="gif on students working on computers" />
+      <section className="description-container">
+        <h1>IronHub: Your Digital Campus Forum</h1>
+        <p>
+          IronHub is where Ironhack students connect, share, and collaborate.
+          Discover peer posts, manage your own, and stay engaged with your
+          courses. Sign up or log in to start exploring today!
+        </p>
+        <p>Made for students by students.</p>
+        <button id="about-btn" onClick={handleNavigate}>
+          Discover Our Story.
+        </button>
+      </section>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
