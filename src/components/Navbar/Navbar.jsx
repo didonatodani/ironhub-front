474afb5a338334bc7cdd3dcef7ce5d3884ccd4b3
Peedee01 <piethein@schouten.nl>
2024-12-03 15:51:50 +0100

    navbar base

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import Logo from "../../assets/IronHub.svg";
 import "./Navbar.css"
 
-function Navbar(){
-    return(
-        <h1>Navbar</h1>
-    )
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="logo">
+          <NavLink>
+            <img src={Logo} alt="ironhub-logo" />
+          </NavLink>
+        </div>
+        <ul className="links">
+          <NavLink to="/">
+            <li>About</li>
+          </NavLink>
+          <NavLink>
+            <li>Signup</li>
+          </NavLink>
+          <NavLink>
+            <li>Login</li>
+          </NavLink>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
