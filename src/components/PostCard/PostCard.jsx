ef268186ea6fe651494aa729080d8330052bbe8f
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-12-03 18:43:30 +0100

    posts page and post cards data fetched and some styles done

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,9 +1,20 @@
-import "./PostCard.css"
+import "./PostCard.css";
 
-function PostCard(){
-    return(
-        <h1>Post Card</h1>
-    )
+function PostCard({ post }) {
+  const { created, description, likes, link, name, picture, title } = post;
+
+  const formattedDate = new Date(created).toLocaleDateString('en-GB');
+
+  return (
+    <article className="post-card">
+      <div className="post-card-header">
+        <span>{name.name}</span>
+        <span>{formattedDate}</span>
+      </div>
+      <h2>{title}</h2>
+      <p id="post-card-description">{description}</p>
+    </article>
+  );
 }
 
-export default PostCard
+export default PostCard;
--- src/pages/PostsPage/PostsPage.jsx
+++ src/pages/PostsPage/PostsPage.jsx
@@ -1,7 +1,33 @@
-import "./PostsPage.css"
+import { useState, useEffect } from "react";
+import PostCard from "../../components/PostCard/PostCard";
+import "./PostsPage.css";
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 function PostsPage() {
-  return (<h1>Posts Page</h1>);
+  const [postsArray, setPostsArray] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/posts`)
+      .then((res) => {
+        console.log(res.data);
+        setPostsArray(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <section className="posts-container">
+      {postsArray.map((post, index) => {
+        return <PostCard key={index} post={post}/>;
+      })}
+    </section>
+  );
 }
 
 export default PostsPage;
