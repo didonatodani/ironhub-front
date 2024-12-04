2246e0f6520ded015f6d4ff9901eb429ec35e3c1
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-12-04 12:14:21 +0100

    sign-up form started

--- src/components/Forms/SignupForm.jsx
+++ src/components/Forms/SignupForm.jsx
@@ -2,7 +2,22 @@ import "./GeneralFormStyles.css"
 
 function SignupForm(){
     return(
-        <h1>Sign up form</h1>
+        <section className="sign-up-section">
+            <form>
+                <label htmlFor="fullName"></label>
+                <input type="text" name="fullName" id="fullName" />
+                <label htmlFor="course"></label>
+                <input type="text" name="course" id="course" />
+                <label htmlFor="linkedin"></label>
+                <input type="text" name="linkedin" id="linkedin" />
+                <label htmlFor="schedule"></label>
+                <input type="text" name="schedule" id="schedule" />
+                <label htmlFor="languages"></label>
+                <input type="text" name="languages" id="languages" />
+                <label htmlFor="email"></label>
+                <input type="text" name="email" id="email" />
+            </form>
+        </section>
     )
 }
 
