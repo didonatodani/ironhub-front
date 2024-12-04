016c5bf76c3e1c02505ec14475dad176507cc041
Peedee01 <piethein@schouten.nl>
2024-12-04 20:39:06 +0100

    errorpage done

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,17 @@
-import "./ErrorPage.css"
+import "./ErrorPage.css";
+import Lucas from "../../assets/Lucas.png";
+import { NavLink } from "react-router-dom";
 
 function ErrorPage() {
   return (
-  <h1>Error</h1>
-)
+    <div className="error-container">
+      <h1>Oops, our dog ate this page...</h1>
+      <img width={280} src={Lucas} alt="lucas-image" />
+      <NavLink to={"/"}>
+        <button className="back-to-home-button">Return to home</button>
+      </NavLink>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
