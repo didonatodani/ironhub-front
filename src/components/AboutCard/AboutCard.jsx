eb70467754dffe1aaf4463b7db663f032a88879c
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-12-09 13:11:40 +0100

    about page stylings done

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -1,17 +1,29 @@
-import githubLogo from "../../assets/github-logo.svg"
-import linkedinLogo from "../../assets/linkedin-logo.svg"
-import "./AboutCard.css"
+import { useState } from "react";
+import githubLogo from "../../assets/github-logo.svg";
+import linkedinLogo from "../../assets/linkedin-logo.svg";
+import "./AboutCard.css";
 
 function AboutCard({ member }) {
   const { name, linkedin, github, description } = member;
+  const [hideDescription, setHideDescription] = useState(true);
+
   return (
     <article className="about-card">
-      <div className={`img-div ${name.toLowerCase().slice(0,3)}`}></div>
+      <div
+        className={`img-div ${
+          hideDescription && name.toLowerCase().slice(0, 3)
+        }`}
+       onClick={()=>{setHideDescription(!hideDescription)}}>
+        <p className={`${hideDescription && "hide"}`}>{description}</p>
+      </div>
       <h2>{name}</h2>
       <div className="about-descr-div">
-        <p>{description}</p>
-        <a href={linkedin}><img src={linkedinLogo} alt="linkedin logo" className="social-logo"/></a>
-        <a href={github}><img src={githubLogo} alt="github logo" className="social-logo"/></a>
+        <a href={linkedin} target="_blank">
+          <img src={linkedinLogo} alt="linkedin logo" className="social-logo" />
+        </a>
+        <a href={github} target="_blank">
+          <img src={githubLogo} alt="github logo" className="social-logo" />
+        </a>
       </div>
     </article>
   );
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -5,6 +5,18 @@ import AboutCard from "../../components/AboutCard/AboutCard";
 function AboutPage() {
   return (
     <section className="about-section">
+      <div className="title-section">
+        <h2>MADE FOR STUDENTS BY STUDENTS</h2>
+        <p>
+          Welcome to IronHub, the ultimate forum for Ironhack students! This
+          platform is designed to bring together students from Web Development,
+          Data Analytics, and UX/UI courses. Share knowledge, connect with
+          peers, and grow your skills through collaboration and discussion. Join
+          us and make the most out of your Ironhack experience!{" "}
+          <br/>
+          <span id="about-span">Click on the images to find more about us!</span>
+        </p>
+      </div>
       {teamData.map((member, index) => {
         return <AboutCard key={index} member={member} />;
       })}
