6c8722efe4e51083713d17c0c6b3b5f092876792
Nigel Ferreres <nferrefe@gmail.com>
2024-12-10 15:03:26 +0100

    merge test1 cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -17,14 +17,14 @@ describe("IronHub", () => {
   //   cy.get(".logo").click();
   // });
 
-  it("Log in and visit all posts page", () => {
-    cy.visit("http://localhost:5173/");
-    cy.get("#nav-login").contains("Login").click();
-    cy.get('input[name="email"]').type(Cypress.env("email"));
-    cy.get('input[name="password"]').type(Cypress.env("password"));
-    cy.get('button.primary-button[type="submit"]').click();
-    cy.get(".logo").click();
-  });
+  // it("Log in and visit all posts page", () => {
+  //   cy.visit("http://localhost:5173/");
+  //   cy.get("#nav-login").contains("Login").click();
+  //   cy.get('input[name="email"]').type(Cypress.env("email"));
+  //   cy.get('input[name="password"]').type(Cypress.env("password"));
+  //   cy.get('button.primary-button[type="submit"]').click();
+  //   cy.get(".logo").click();
+  // });
   it("Create a post, search for it and see details", ()=> {
     cy.visit("http://localhost:5173/");
     cy.get("#nav-login").contains("Login").click();
